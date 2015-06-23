aadab7047ddbdd7ba4b790a84a8610a4e202f341
Alexander Otavka <zotavka@gmail.com>
2015-06-22 18:04:36 -0700

    Add lesson 2 complete.

--- lesson1/lesson1step1.py
+++ lesson1/lesson1step1.py
@@ -11,7 +11,7 @@ TASKS:
     - Change the image to "puppy.jpg".
 '''
 
-# Pyglet allows us to create windows and draw things to those 
+# Pyglet allows us to create windows and draw things to those
 # windows.  It isn't build into python, so we need to import it in
 # order to use it.
 import pyglet
@@ -20,11 +20,11 @@ import pyglet
 # Create a new window, using pyglet, and name it `window`.
 window = pyglet.window.Window()
 
-# Load the image "kitten.jpg" with pyglet, and name it 
+# Load the image "kitten.jpg" with pyglet, and name it
 # `kitten_image`.
 kitten_image = pyglet.image.load('kitten.jpg')
 
-# When `window` decides it needs to redraw (flip to a new page in 
+# When `window` decides it needs to redraw (flip to a new page in
 # the flip book), it will run the indented code.
 @window.event
 def on_draw():
---
+++ lesson2/lesson2complete.py
@@ -0,0 +1,65 @@
+'''Lesson 2 - complete.
+
+Example of a completed lesson 2.
+'''
+
+from pyglet.window import Window
+from pyglet.image import load as load_image
+from pyglet.sprite import Sprite
+from pyglet.clock import schedule_interval
+from pyglet.app import run
+
+from physics.collision import get_exit_direction
+from physics.vector import Vector
+from physics.rect import Rect
+
+
+window = Window()
+
+paddle_image = load_image('paddle.png')
+paddle_sprite = Sprite(paddle_image, 0, 10)
+
+ball_image = load_image('ball.png')
+ball_sprite = Sprite(ball_image, 10, 300)
+ball_velocity = Vector()
+ball_velocity.x = 50
+ball_velocity.y = -50
+
+left_wall = Rect.new(0, 0, 0, window.height)
+top_wall = Rect.new(0, window.height, window.width, 0)
+right_wall = Rect.new(window.width, 0, 0, window.height)
+
+walls = [left_wall, top_wall, right_wall, paddle_sprite]
+
+@window.event
+def on_draw():
+    window.clear()
+    paddle_sprite.draw()
+    ball_sprite.draw()
+
+@window.event
+def on_mouse_motion(mouse_x, mouse_y, dx, dy):
+    right_side = window.width - paddle_sprite.width / 2
+    left_side = paddle_sprite.width / 2
+
+    if mouse_x > right_side:
+        mouse_x = right_side
+    if mouse_x < left_side:
+        mouse_x = left_side
+
+    paddle_sprite.x = mouse_x - paddle_sprite.width / 2
+
+def update_ball(dt):
+    delta_pos = Vector()
+    for wall in walls:
+        delta_pos += get_exit_direction(ball_sprite, wall)
+    ball_velocity.copysign(delta_pos)
+    delta_pos += ball_velocity * dt
+
+    ball_sprite.x += delta_pos.x
+    ball_sprite.y += delta_pos.y
+
+schedule_interval(update_ball, 1.0/60)
+
+
+run()
